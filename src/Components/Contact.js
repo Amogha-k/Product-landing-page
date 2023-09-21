dae06385ca648f3c926490fe5d17eee4e8e5f1a7
Amogha k <amoghak2003@gmail.com>
2023-09-21 14:04:30 +0530

    update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,16 +6,8 @@ const Contact = () => {
       <h1 className="primary-heading">Any Questions ?</h1>
       <h1 className="primary-heading">Let me help you</h1>
       <div className="contact-form-container">
-      <form style={{ display: 'flex', alignItems: 'center' }}>
-  <div style={{ flex: 1, marginRight: 'auto' }}>
-    <input type="text" placeholder="yourmail@gmail.com" />
-  </div>
-  <button className="secondary-button" style={{ flex: 1, marginLeft: '250px' }}>Submit</button>
-</form>
-
-
-
-
+        <input type="text" placeholder="yourmail@gmail.com" />
+        <button type="submit" className="secondary-button">Submit</button>
       </div>
     </div>
   );
